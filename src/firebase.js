23cda523bb0a07f303b7fa93ac7cbb09e26010bb
ppby <salswnss@gmail.com>
2020-07-26 14:05:20 +0900

    Set firebase initializeApp

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+// firebase 설정 (프로젝트 설정에 Firebase SDK snippet에 나옴)
+const firebaseConfig = {
+  apiKey: 'AIzaSyAAH3yObv6DkcR6WAQe4tkXOsbyrAM_2XM',
+  authDomain: 'ppby-chat.firebaseapp.com',
+  databaseURL: 'https://ppby-chat.firebaseio.com',
+  projectId: 'ppby-chat',
+  storageBucket: 'ppby-chat.appspot.com',
+  messagingSenderId: '251164832636',
+  appId: '1:251164832636:web:cfce95e38eb8ab173baaf9',
+};
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
